6a3f9dfeaad6fea153aab4edd89dbc52865c055c
kiuver39 <forrexplug@gmail.com>
2024-10-15 09:21:00 -0700

    fixing

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+'''Asynchronous Python
+'''
+from typing import List
+
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
-    """Coroutine that collects 10 random
-    numbers using async comprehension.
-    """
-
-    random = [num async for num in async_generator()]
-    return random
+async def async_comprehension() -> List[float]:
+    '''make a list of 10 num from imported generator
+    '''
+    return [num async for num in async_generator()]
