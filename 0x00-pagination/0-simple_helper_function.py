f3f3514646c612ed65e4c23643ec2232b77d3b2d
kiuver39 <forrexplug@gmail.com>
2024-10-28 05:46:33 -0700

    testing

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+"""index_range function"""
+
 
 def index_range(page: int, page_size: int) -> tuple:
-
-    page_start_index = (page - 1) * page_size
-    end_index = page_start_index + page_size
-
-    return (page_start_index, end_index)
+    """return a tuple of size two containing a start index and an end index"""
+    start = (page - 1) * page_size
+    return (start, start + page_size)
